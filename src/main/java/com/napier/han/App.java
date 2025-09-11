240e860d2f746ac5d79ea77923b03bb30df6c8e3
HaymarnYH <haymarn1299@gmail.com>
2025-09-11 11:41:07 +0630

    Added Hello world1

--- src/main/java/com/napier/han/App.java
+++ src/main/java/com/napier/han/App.java
@@ -3,6 +3,6 @@ package com.napier.han;
 public class App {
     public static void main(String[] args)
     {
-        System.out.println("Boo yah!");
+        System.out.println("Boo yah");
     }
 }
